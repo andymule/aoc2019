588f79e764cf2011b87e3a1ac46c3042606d1905
Andy Mule <andymule@gmail.co>
2019-11-30 20:04:17 +0100

    pre-set my things bb

---
+++ aoc2019/day1/day1.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	int jk = 6;
+	float jj = 69;
+	print("helo");
+	print(69);
+	print(jk);
+	print(jj);
+    return 0;
+}
