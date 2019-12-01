0caff4cef4ea29ee454379519277b79ca4c86c6a
Andy Mule <andymule@gmail.co>
2019-12-01 07:03:20 +0100

    welcom bak day 1 bb boi

--- day1.py
+++ day1.py
@@ -1,2 +1,12 @@
-print("helo")
- 
+import math
+filepath = 'day1.txt'
+counter = 0
+with open(filepath) as fp:
+ for cnt, line in enumerate(fp):
+  start = math.floor(int(line)/3)-2
+  counter += start
+  while (start > 0):
+   start = math.floor(int(start)/3)-2
+   if (start > 0):
+    counter += start
+print(counter)
