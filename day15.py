e0842558fc14914806453882f832b75eb7621779
andymule <andymule@gmail.com>
2019-12-16 16:08:10 +0100

    i15

--- day15.py
+++ day15.py
@@ -1,16 +1,29 @@
-import math, sys, pprint, os
-from sortedcontainers import SortedDict
+import sys
+import time, os
 from collections import defaultdict
+import json
 import keyboard
+import networkx as nx
+from sortedcontainers import SortedDict
+import curses
 
-filepath = "day15.txt"
+# from pynput import keyboard
+
+manualStepper = 0
+yPos = 0
+xPos = 0
+blankChar = " "
+MAP = defaultdict(lambda: defaultdict(lambda: blankChar))
+MAP[yPos][xPos] = '@'
+goalX = -69
+goalY = -69
+startX = 0
+startY = 0
 
 words = SortedDict()
-MAP = defaultdict(lambda: defaultdict(lambda: " "))
 relptr = 0
 ptr = 0
-_input = 1
-lastcode = 0
+filepath = "day15.txt"
 
 codes = {
     1: lambda a, b: a + b,
@@ -54,7 +67,7 @@ def RefOrVal(mode, get):
         sys.exit(1)
 
 
-def next(p):
+def nextPtr(p):
     global words, ptr
     ptr += 1
     try:
@@ -65,37 +78,52 @@ def next(p):
 
 
 def DrawMap():
-    global MAP
+    global MAP, goalX, goalY, manualStepper, startX, startY, blankChar, xPos, yPos
     os.system('cls' if os.name == 'nt' else 'clear')  # clears screen
-    print('\n'*80) # prints 80 line breaks
+    # print('\n')  # * 10)  # prints 80 line breaks
     yStart = sorted(MAP.keys())[0]
     ySpan = len(MAP.keys())
     minX = 0
     maxX = 0
-    for y in range(yStart, ySpan):
+    if goalX != -69 and goalY != -69:
+        MAP[goalY][goalX] = '$'
+    MAP[startY][startX] = 'X'
+    for y in range(yStart, yStart + ySpan):
         for x in MAP[y].keys():
-            if x < minX:
+            if MAP[y][x] != blankChar and x < minX:
                 minX = x
-            if x > maxX:
+            if MAP[y][x] != blankChar and x > maxX:
                 maxX = x
-    for y in range(yStart, ySpan):
+    for y in range(yStart, yStart + ySpan):
         s = ""
-        for x in range(minX, maxX+1):
-            s += MAP[y][x]
+        for x in range(minX, maxX + 1):
+            # if x == startX and y == startY: # and xPos != startX and yPos != startY:
+            # s += 'X'
+            if x == xPos and y == yPos:
+                s += '@'
+            # elif x == goalX and y == yPos and (goalX != -69 and goalX != goalX):
+            # s += '$'
+            else:
+                s += MAP[y][x]
         print(s)
-
-MAP[0][0] = '@'
+    print()
+    print(manualStepper)
 
 
 def runDay15():
-    global words, lastcode, relptr, ptr
+    global words, relptr, ptr, xPos, yPos, goalX, goalY, MAP, manualStepper
     with open(filepath) as file:
-        alldata = file.read().replace("\n", "")
+        alldata: str = file.read().replace("\n", "")
         wordsraw = [int(n) for n in alldata.split(",")]  # ints
         for n in range(0, len(wordsraw)):
             words[n] = wordsraw[n]
+        move = 1
+        lastSignal = 0
+        realMove = False
+        # takeinputnow = False
+        takeinputcount = 0
         while True:
-            instruct = str(next(ptr))  # string
+            instruct = str(nextPtr(ptr))  # string
             op = int(instruct[-2:])
             m1 = 0
             m2 = 0
@@ -107,15 +135,59 @@ def runDay15():
             if len(instruct) > 4:  # mode 3
                 m3 = int(instruct[-5:-4])
             if op == 1 or op == 2:  # arithmetic
-                p1 = RefOrVal(m1, next(ptr))
-                p2 = RefOrVal(m2, next(ptr))
-                at = int(next(ptr))
+                p1 = RefOrVal(m1, nextPtr(ptr))
+                p2 = RefOrVal(m2, nextPtr(ptr))
+                at = int(nextPtr(ptr))
                 newval = run(op, p1, p2)
                 assign(m3, at, newval)
             if op == 3:  # assign
-                move = 1
-                DrawMap()
-                key = keyboard.read_key()
+                if takeinputcount % 9 == 0:
+                    takeinputcount = 1
+                    DrawMap()
+                    key = keyboard.read_key(True)
+                    realMove = True
+                    while not (key == 'a' or key == 'w' or key == 's' or key == 'd'):
+                        if key == 'q':
+                            manualStepper = 0
+                            DrawMap()
+                        if key == 'o':
+                            with open("map.json", 'w') as outfile:
+                                json.dump(MAP, outfile)
+                        if key == 'l':
+                            with open("map.json") as infile:
+                                NEWMAP = json.load(infile)
+                                NEWMAP = {int(k): v for k, v in NEWMAP.items()}
+                                for k in NEWMAP.keys():
+                                    NEWMAP[k] = {int(k): v for k, v in NEWMAP[k].items()}
+                                MAP = defaultdict(lambda: defaultdict(lambda: blankChar))
+                                for y, d in NEWMAP.items():
+                                    for x, v in NEWMAP[y].items():
+                                        MAP[y][x] = NEWMAP[y][x]
+                                DrawMap()
+                        key = keyboard.read_key(True)
+                    time.sleep(.01)
+                else:
+                    takeinputcount += 1
+                    if takeinputcount == 2:
+                        key = 'a'
+                    if takeinputcount == 3:
+                        if lastSignal != 0:
+                            key = 'd'
+                    if takeinputcount == 4:
+                        key = 'd'
+                    if takeinputcount == 5:
+                        if lastSignal != 0:
+                            key = 'a'
+                    if takeinputcount == 6:
+                        key = 'w'
+                    if takeinputcount == 7:
+                        if lastSignal != 0:
+                            key = 's'
+                    if takeinputcount == 8:
+                        key = 's'
+                    if takeinputcount == 9:
+                        if lastSignal != 0:
+                            key = 'w'
                 if key == 'a':
                     move = 3
                 if key == 'w':
@@ -124,45 +196,192 @@ def runDay15():
                     move = 2
                 if key == 'd':
                     move = 4
-                assign(m1, next(ptr), move)
+                assign(m1, nextPtr(ptr), move)
                 # assign(m1, next(ptr), _input)
             if op == 4:  # output
-                p1 = RefOrVal(m1, next(ptr))
-                lastcode = p1
+                p1 = RefOrVal(m1, nextPtr(ptr))
+                lastSignal = p1
+                tempX = xPos
+                tempY = yPos
+                if move == 3:
+                    tempX -= 1
+                if move == 1:
+                    tempY -= 1
+                if move == 2:
+                    tempY += 1
+                if move == 4:
+                    tempX += 1
+                if p1 == 0:
+                    MAP[tempY][tempX] = "#"
+                    if realMove:
+                        realMove = False
+                if p1 == 1:  # moved in direction
+                    # MAP[tempY][tempX] = "@"
+                    MAP[yPos][xPos] = "."
+                    xPos = tempX
+                    yPos = tempY
+                    if realMove:
+                        manualStepper += 1
+                        realMove = False
+                if p1 == 2:
+                    MAP[tempY][tempX] = "$"
+                    MAP[yPos][xPos] = "."
+                    xPos = tempX
+                    yPos = tempY
+                    goalX = xPos
+                    goalY = yPos
+                    if realMove:
+                        manualStepper += 1
+                        realMove = False
 
-                # print(p1)
+            # print(p1)
             if op == 5:  # branch not zero
-                p1 = RefOrVal(m1, next(ptr))
-                p2 = RefOrVal(m2, next(ptr))
+                p1 = RefOrVal(m1, nextPtr(ptr))
+                p2 = RefOrVal(m2, nextPtr(ptr))
                 if p1 != 0:
                     ptr = p2
             if op == 6:  # branch zero
-                p1 = RefOrVal(m1, next(ptr))
-                p2 = RefOrVal(m2, next(ptr))
+                p1 = RefOrVal(m1, nextPtr(ptr))
+                p2 = RefOrVal(m2, nextPtr(ptr))
                 if p1 == 0:
                     ptr = p2
             if op == 7:  # less than
-                p1 = RefOrVal(m1, next(ptr))
-                p2 = RefOrVal(m2, next(ptr))
-                at = int(next(ptr))
+                p1 = RefOrVal(m1, nextPtr(ptr))
+                p2 = RefOrVal(m2, nextPtr(ptr))
+                at = int(nextPtr(ptr))
                 if p1 < p2:
                     assign(m3, at, 1)
                 else:
                     assign(m3, at, 0)
             if op == 8:  # equal to
-                p1 = RefOrVal(m1, next(ptr))
-                p2 = RefOrVal(m2, next(ptr))
-                at = int(next(ptr))
+                p1 = RefOrVal(m1, nextPtr(ptr))
+                p2 = RefOrVal(m2, nextPtr(ptr))
+                at = int(nextPtr(ptr))
                 if p1 == p2:
                     assign(m3, at, 1)
                 else:
                     assign(m3, at, 0)
             if op == 9:  # move ref
-                p1 = RefOrVal(m1, next(ptr))
+                p1 = RefOrVal(m1, nextPtr(ptr))
                 relptr += p1
             if op == 99:  # exit
                 print("EXITING")
                 sys.exit(0)
 
 
-runDay15()
+# runDay15()
+
+wholemap = (
+" ### ### ############# # ########### ### \n"
+"#...#...#.............#.#...........#...#\n"
+"#.#.#.#.###.#########.#.#.###.#####.###.#\n"
+"#.#...#...#...#.....#.#.#.#...#.........#\n"
+"#.#######.###.#.#####.#.#.#.###########.#\n"
+"#.#...........#.......#.#.#.......#...#.#\n"
+"#.###.#########.#######.#.#######.#.#.#.#\n"
+"#...#.#.......#.#...............#.#.#...#\n"
+" ##.###.#####.#.#.###########.###.#.#### \n"
+"#...#...#...#.#.#...#...#.....#...#.#...#\n"
+"#.###.###.#.#.#.#####.#.#.#####.###.###.#\n"
+"#...#...#.#.#.#...#...#.#...#...#.......#\n"
+"#.#.###.###.#.###.#.###.#####.########## \n"
+"#.#...#.#...#.#...#.#.#.......#...#.....#\n"
+" ####.#.#.###.#.###.#.###.#####.#.#.###.#\n"
+"#.....#.#...#.#.....#...#.#...#.#.....#.#\n"
+"#.#####.#.#.#.#######.#.#.#.#.#.#######.#\n"
+"#.#.....#.#.#...#.....#...#.#...#.......#\n"
+"#.#...........#.......#.#.#.......#...#.#\n"
+"#.###.#########.#######.#.#######.#.#.#.#\n"
+"#...#.#.......#.#...............#.#.#...#\n"
+" ##.###.#####.#.#.###########.###.#.#### \n"
+"#...#...#...#.#.#...#...#.....#...#.#...#\n"
+"#.###.###.#.#.#.#####.#.#.#####.###.###.#\n"
+"#...#...#.#.#.#...#...#.#...#...#.......#\n"
+"#.#.###.###.#.###.#.###.#####.########## \n"
+"#.#...#.#...#.#...#.#.#.......#...#.....#\n"
+" ####.#.#.###.#.###.#.###.#####.#.#.###.#\n"
+"#.....#.#...#.#.....#...#.#...#.#.....#.#\n"
+"#.#####.#.#.#.#######.#.#.#.#.#.#######.#\n"
+"#.#.....#.#.#...#.....#...#.#...#.......#\n"
+"#.#.#######.###.###########.#####.###### \n"
+"#.#.#.....#...#.#...........#...#.#.....#\n"
+"#.#.#.#.#.#.#.#.#.#####.#####.###.#.###.#\n"
+"#.#.#.#.#...#.#.#.#...#...#.....#.#.#...#\n"
+"#.#.###.#####.#.#.#.#####.#####.#.#.#.## \n"
+"#.#...#.....#.#...#.#...#.#.....#.#.#...#\n"
+"#.###.#.###.#####.#.#.###.#.#.###.#####.#\n"
+"#.....#.#...#...#.#.#.......#.#...#.....#\n"
+"#.#####.#.###.#.###.###########.###.#### \n"
+"#...#...#...#.#.....#...........#.......#\n"
+" ##.###.###.#.#######.###########.#####.#\n"
+"#.#...#.#...#.........#...#.....#.#.....#\n"
+"#.###.###.#.###########.###.#.###.#.###.#\n"
+"#.#...#...#.#...#.....#.#...#.....#.#...#\n"
+"#.#.###.###.#.#.#.###.#.#.#########.#.## \n"
+"#...#...#.#...#.#.#...#.#.#......$#.#...#\n"
+"#.###.###.#####.#.#.###.#.###.#.###.###.#\n"
+"#.#...#.......#...#.#.........#.#...#.#.#\n"
+"#.###.#.###.#######.#########.###.###.#.#\n"
+"#.#...#.#.#.......#...#.....#.#...#.....#\n"
+"#.#.###.#.#######.###.#.###.###.###.#### \n"
+"#...#...........#.......#.......#.......#\n"
+" ### ########### ####### ####### ####### \n")
+
+
+# print(wholemap)
+# 41 wide, 54 tall
+
+def at(y, x):
+    global wholemaplist
+    pos = 42 * y + x
+    return wholemaplist[pos]
+
+
+def make(y, x, b):
+    global wholemaplist
+    pos = 42 * y + x
+    wholemaplist[pos] = b
+    jk = 69
+
+minutesPassed = 0
+wholemaplist = list(wholemap)
+mymiddleX = 21
+mymiddleY = 34
+ox = 12 + mymiddleX
+oy = 12 + mymiddleY
+oxy = set()
+oxy.add((oy, ox))
+
+make(oy,ox, '☺')
+
+def expand():
+    global oxy, wholemaplist, minutesPassed
+    newoxy = set()
+    changed = False
+    for o in oxy:
+        if o[0] < 54 and at(o[0] + 1, o[1]) == '.':  # lower
+            newoxy.add((o[0] + 1, o[1]))
+        if o[0] > 1 and at(o[0] - 1, o[1]) == '.':  # upper
+            newoxy.add((o[0] - 1, o[1]))
+        if o[1] > 1 and at(o[0], o[1] - 1) == '.':  # left
+            newoxy.add((o[0], o[1] - 1))
+        if o[1] < 41 and at(o[0], o[1] + 1) == '.':  # right
+            newoxy.add((o[0], o[1] + 1))
+    for o in newoxy:
+        changed = True
+        make(o[0], o[1], '♥')
+    if not changed:
+        print(minutesPassed)
+        sys.exit(0)
+    else:
+        changed = False
+        minutesPassed += 1
+    oxy.update(newoxy)
+    
+#376 too high, so is 374
+    # print("".join(wholemaplist))
+    jk = 69
+
+while True:
+    expand()
+# make(o[0] + 1, o[1], newmap, '$')
